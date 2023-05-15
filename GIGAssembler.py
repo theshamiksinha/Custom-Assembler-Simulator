3911f34e427f996f49d9955aa60bfe041945554d
shamiksinha123 <118443852+shamiksinha123@users.noreply.github.com>
2023-05-15 23:28:18 +0530

    Update GIGAssembler.py

--- GIGAssembler.py
+++ GIGAssembler.py
@@ -155,3 +155,169 @@ def error_checker(filename):
 
                                 else:
                                         li.append(f"Error in line {instruction.index(i) + 1}: A register can only be of type R0/1/2/3/4/5/6")
+                elif i[0]=="mov":
+                        t=1
+                        if len(i)!=3:
+                                li.append(f"Error in Line {instruction.index(i) + 1}: mov must contain 2 parameters ")
+                        else:
+                                
+                                if not(i[2][1::].isdigit()):
+                                        if (i[1] in register.keys() and i[1]!="FLAGS"  and i[2] in register.keys()):
+                                                continue
+                                        else:
+                                               li.append(f"Error in line {instruction.index(i) + 1}: Register can be of type R0/1/2/3/4/5/6")
+                                               continue
+                                        
+                                else:
+                                        if (i[1] in register.keys() and i[1]!="FLAGS" and (0 <= int(i[2][1:]) and int(i[2][1:]) <= 127) and i[2][0]=="$"):
+                                                continue
+                                        else:
+                                                li.append(f"Error in line {instruction.index(i) + 1}: int value not defined / int value not in range / register name invalid ")
+                                                
+
+                elif i[0]=="ld" or i[0]=="st":
+                        t=1
+                        if len(i)!=3:
+                                li.append(f"Error in Line {instruction.index(i) + 1}: ld and st must contain 2 parameters ")
+                        else:
+                                if (i[1] in register.keys() and i[1]!="FLAGS" and i[2] in variables):
+                                        continue
+                                else:
+                                        if i[2] not in variables:
+                                                li.append(f"Error in line {instruction.index(i) + 1}: No variable named '{i[2]}' ")
+                                        if i[1] not in register.keys():
+                                                li.append(f"Error in line {instruction.index(i) + 1}: No register named '{i[1]}' ")
+                
+
+                elif i[0]=="mul" or i[0]=="div":
+                        t=1
+                        if i[0]=="mul":
+                                if len(i)!=4:
+                                        li.append(f"Error in Line {instruction.index(i) + 1}: mul must contain 3 parameters ")
+                                else:
+                                        if i[1] in register.keys() and i[1]!="FLAGS" and i[2] in register.keys() and i[2]!="FLAGS" and i[3] in register.keys() and i[3]!="FLAGS" :
+                                                continue
+                                        else:
+                                                li.append(f"Error in line {instruction.index(i) + 1}: Registers can only be of type R0/1/2/3/4/5/6")
+                                                
+                        elif i[0]=="div":
+                                if len(i)!=3:
+                                        li.append(f"Error in Line {instruction.index(i) + 1}: div must contain 3 parameters ")
+                                else:
+                                        if i[1] in register.keys()  and i[1]!="FLAGS" and i[2] in register.keys() and i[2]!="FLAGS":
+                                                continue
+                                        else:
+                                                li.append(f"Error in line {instruction.index(i) + 1}: Registers can only be of type R0/1/2/3/4/5/6")
+                
+                elif i[0]=="rs" or i[0]=="ls":
+                        t=1
+                        # print(1)
+                        if len(i)!=3:
+                                li.append(f"Error in Line {instruction.index(i) + 1}: rs / ls must contain 2 parameters ")
+                        else:
+                                # print(i[2])
+                                if i[1] in register.keys() and i[1]!="FLAGS" and (0 <= int(i[2][1::]) and int(i[2][1::]) <= 127) :
+                                        continue
+                                else:
+                                        li.append(f"Error in line {instruction.index(i) + 1}: Registers name not valid / int not in range")
+
+                elif i[0]=="xor" or i[0]=="or" or i[0]=="and":
+                        t=1
+                        if len(i)!=4:
+                                li.append(f"Error in Line {instruction.index(i) + 1}: xor/or/and must contain 3 parameters ")
+                        else:
+                                if i[1] in register.keys() and i[1]!="FLAGS" and i[2] in register.keys() and i[2]!="FLAGS" and i[3] in register.keys() and i[3]!="FLAGS" :
+                                        continue
+                                else:
+                                        li.append(f"Error in line {instruction.index(i) + 1}: Registers can only be of type R0/1/2/3/4/5/6")
+
+                elif i[0]=="not" or i[0]=="cmp":
+                        t=1
+                        if len(i)!=3:
+                                li.append(f"Error in Line {instruction.index(i) + 1}: not / cmp must contain 2 parameters ")
+                        else:
+                                if i[1] in register.keys() and i[1]!="FLAGS" and i[2] in register.keys() and i[2]!="FLAGS" :
+                                        continue
+                                else:
+                                        li.append(f"Error in line {instruction.index(i) + 1}: Registers can only be of type R0/1/2/3/4/5/6")
+
+                elif i[0]=="jmp" or i[0]=="jlt" or i[0]=="jgt" or i[0]=="je":
+                        t=1
+                        if len(i)!=2:
+                                li.append(f"Error in Line {instruction.index(i) + 1}: jump statements must contain 1 label ")
+                        else:
+                                labels.append(i[1])
+                                w=1
+                                
+                                for q in instruction:
+                                        
+                                        if i[1]+":" in q:
+                                               w+=1
+                                        
+                                if w<2:
+                                       li.append(f"Error in Line {instruction.index(i) + 1}: No label named '{i[1]}' ")
+
+                                               
+                                
+
+                elif i[0] in labels or i[0][-1]==":":
+                        labels.append(i[0])
+                        t=1
+                        if i[0][-1]!=":":
+                                li.append(f"Error in Line {instruction.index(i) + 1}: labels must end with ':' and no spaces are allowed in between ")
+                        else:
+                                j=[a for a in i[1:]]
+                                ###
+                                if j[0][-1] == ":" and len(j) != 0:
+                                        
+                                        if j[0][:-1] in labellll.keys():
+                                                li.append("two label with same name")
+                                                print("two label with same name")
+                                        else:
+                                                print("in in side")
+                                                labellll[j[0][0:-1]]=binn(instruction.index(i)-len(variables),7)
+                                if len(j)==0:
+                                # pass
+                                        continue
+
+                                elif j[0]=="var":
+                                        if t==1:
+                                                li.append(f"Error in line {instruction.index(i)+1}:Variables must be declared at the very beginning")
+                                                continue
+                                        else:
+                                                if len(j)!=2:
+                                                        li.append(f"Error in line {instruction.index(i) + 1}: Varibale definition not proper")
+                                                else:
+                                                        variables.append(j[1])
+                                                        
+
+                                elif j[0]=="add" or j[0]=="sub":
+                                        t=1
+                                        if len(j)!=4:
+                                                li.append(f"Error in Line {instruction.index(i) + 1}: add must contain 3 parameters ")
+                                                if (len(j)==3 and j[1] in register.keys() and j[1]!="FLAGS" and ((j[2][1:])).isdigit() and j[2][0]=="$"):
+                                                        li.append(f"Error in Line {instruction.index(i) + 1}: cannot add value of register and an integer ")
+                                                        #isa doesnt support addition of int and reg values unlike in the test case 1.
+                                                
+
+                                        else:
+                                                if j[1] in register.keys() and j[1]!="FLAGS" and j[2] in register.keys() and j[2]!="FLAGS" and j[3] in register.keys() and j[3]!="FLAGS"  :
+                                                        continue
+
+                                                else:
+                                                        li.append(f"Error in line {instruction.index(i) + 1}: A register can only be of type R0/1/2/3/4/5/6")
+                                                        
+                                elif j[0]=="mov":
+                                        t=1
+                                        if not(j[2][1::].isdigit()):
+                                                if (j[1] in register.keys() and j[1]!="FLAGS" and j[2] in register.keys()):
+                                                        continue
+                                                else:
+                                                        li.append(f"Error in line {instruction.index(i) + 1}: Register can be of type R0/1/2/3/4/5/6")
+                                                        continue
+                                                
+                                        else:
+                                                if (j[1] in register.keys() and j[1]!="FLAGS" and (0 <= int(j[2][1:]) and int(j[2][1:]) <= 127) and j[2][0]=="$"):
+                                                        continue
+                                                else:
+                                                        li.append(f"Error in line {instruction.index(i) + 1}: int value not defined / int value not in range / register name invalid ")
