d16072d40418311092bd048d7d3ab6ca1e7c85ba
vs34 <117767455+vs34@users.noreply.github.com>
2023-06-06 21:15:22 +0530

    Update GIGAssembler.py

--- GIGAssembler.py
+++ GIGAssembler.py
@@ -1,4 +1,4 @@
-opcode={"add":"00000","sub":"00001","movI":"00010","movR":"00011","ld":"00100","st":"00101","mul":"00110","div":"00111","rs":"01000","ls":"01001","xor":"01010","or":"01011","and":"01100","not":"01101","cmp":"01110","jmp":"01111","jlt":"11100","jgt":"11101","je":"11111","hlt":"11010"}
+opcode={"add":"00000","sub":"00001","movI":"00010","movR":"00011","ld":"00100","st":"00101","mul":"00110","div":"00111","rs":"01000","ls":"01001","xor":"01010","or":"01011","and":"01100","not":"01101","cmp":"01110","jmp":"01111","jlt":"11100","jgt":"11101","je":"11111","hlt":"11010","movf":"10010","subf":"10001","addf":"10000"}
 
 
 register = {"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
@@ -96,7 +96,7 @@ def error_checker(lines):
                 instruction.append(line)
         ##print(instruction)
 
-        opcodes=["add","sub","mov","ld","st","mul", "div","rs","ls","xor","or","and","not","cmp","jmp","jlt","jgt","je","hlt","var"]
+        opcodes=["add","sub","mov","ld","st","mul", "div","rs","ls","xor","or","and","not","cmp","jmp","jlt","jgt","je","hlt","var","addf","subf","movf"]
 
         variables=[]
 
